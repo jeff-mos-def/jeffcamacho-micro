9f8a448f4acdc77843f4d24fac3b87aefa878040
Jeff <61635774+jeff-mos-def@users.noreply.github.com>
2024-11-25 16:32:39 -0600

    updated sitemap function

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,13 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-micro.vercel.app",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  site: "https://www.jeffcamacho.com", // Your actual website URL
+  integrations: [
+    tailwind(),
+    sitemap(), // Generates your sitemap automatically
+    mdx(),
+    pagefind(),
+  ],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
